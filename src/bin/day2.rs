ebf2b645c2d46688e85af2b4049f47f6cb32e96e
Andrew Zhu <me@andrew-zhu.com>
2021-12-06 00:33:32 -0800

    day 2

---
+++ src/bin/day2.rs
@@ -0,0 +1,50 @@
+use std::io::Read;
+
+fn main() {
+    // I figured out I/O!
+    let mut buffer = String::new();
+    std::io::stdin().read_to_string(&mut buffer).expect("could not read input");
+    println!("Part 1");
+    part1(&buffer);
+    println!("Part 2");
+    part2(&buffer);
+}
+
+fn part1(input: &String) {
+    let directions: Vec<&str> = input.split("\n").collect();
+    let mut horiz = 0i32;
+    let mut vert = 0i32;
+    for direction in directions {
+        match direction.split_once(" ") {
+            Some(("forward", h)) => { horiz += h.parse::<i32>().expect("invalid forward") }
+            Some(("down", v)) => { vert += v.parse::<i32>().expect("invalid down") }
+            Some(("up", v)) => { vert -= v.parse::<i32>().expect("invalid up") }
+            _ => panic!("invalid match")
+        }
+    }
+
+    println!("Horiz: {} Vert: {}", horiz, vert);
+    println!("Result: {}", horiz * vert)
+}
+
+fn part2(input: &String) {
+    let directions: Vec<&str> = input.split("\n").collect();
+    let mut horiz = 0i32;
+    let mut vert = 0i32;
+    let mut aim = 0i32;
+    for direction in directions {
+        let (op, amt) = direction.split_once(" ").expect("invalid instruction");
+        match (op, amt.parse::<i32>().expect("invalid amount")) {
+            ("forward", x) => {
+                horiz += x;
+                vert += aim * x
+            }
+            ("down", x) => { aim += x }
+            ("up", x) => { aim -= x }
+            _ => panic!("invalid match")
+        }
+    }
+
+    println!("Horiz: {} Vert: {}", horiz, vert);
+    println!("Result: {}", horiz * vert)
+}
